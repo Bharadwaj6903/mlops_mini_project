cee731c076d3a90e0e7828ef593733c6ddd89dce
Bharadwaj6903 <bharadwaj6903@gmail.com>
2026-04-09 14:24:04 +0530

    Flask app added

--- notebooks/dagshub_setup.py
+++ notebooks/dagshub_setup.py
@@ -1,6 +1,8 @@
 import dagshub
 
 import mlflow
+
+mlflow.set_tracking_uri("https://dagshub.com/Bharadwaj6903/mlops_mini_project.mlflow")
 dagshub.init(repo_owner='Bharadwaj6903', repo_name='mlops_mini_project', mlflow=True)
 
 with mlflow.start_run():

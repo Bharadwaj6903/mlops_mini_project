c2116f4ed27549126897ad335cb520f02e290dce
Bharadwaj6903 <bharadwaj6903@gmail.com>
2026-04-12 16:30:58 +0530

    ci added

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -7,7 +7,13 @@ import os
 import dagshub
 
 
-dagshub.init(repo_owner='Bharadwaj6903', repo_name='mlops_mini_project', mlflow=True)
+#dagshub.init(repo_owner='Bharadwaj6903', repo_name='mlops_mini_project', mlflow=True)
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
 repo_owner = "Bharadwaj6903"
